34d39d9f9601f4eee37c8052a03c6da3a5e99488
Shameera Rathnayaka <shameerainfo@gmail.com>
2015-11-20 16:09:40 -0500

    Added kafka stream consumer implemenation and update Bootstrap main method to run both consumer and producer

--- src/main/java/org/streaming/spring/Bootstrap.java
+++ src/main/java/org/streaming/spring/Bootstrap.java
@@ -1,21 +1,60 @@
 package org.streaming.spring;
 
+import org.streaming.spring.core.StreamConsumer;
+import org.streaming.spring.kafka.KafkaStreamConsumer;
 import org.streaming.spring.kafka.KafkaStreamProducer;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by syodage on 11/10/15.
  */
 public class Bootstrap {
+    static int count = 0;
 
     public static void main(String[] args) throws Exception {
-        startKafkaProducer();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(() -> new Bootstrap().startKafkaProducer());
+        executorService.submit(() -> new Bootstrap().startKafkaConsumer());
+        while (count < 2) {
+            waitFor(5000);
+        }
+        executorService.shutdown();
     }
 
-    public static void startKafkaProducer() throws Exception {
-        KafkaStreamProducer streamKafkaProducer = new KafkaStreamProducer();
-        streamKafkaProducer.start();
-        waitFor(10000);
-        streamKafkaProducer.stop();
+    private void startKafkaConsumer(){
+        try {
+            String zooKeeper = "localhost:2181";
+            String groupId = "group_1";
+            String topic = "test";
+            int threads = 1;
+
+            StreamConsumer example = new KafkaStreamConsumer(zooKeeper, groupId, topic, threads);
+            example.start();
+
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ie) {
+
+            }
+            example.stop();
+        } catch (Exception e) {
+            System.out.println("Error while consuming kafka streams");
+        }
+        ++count;
+    }
+
+    public void startKafkaProducer() {
+        try {
+            KafkaStreamProducer streamKafkaProducer = new KafkaStreamProducer();
+            streamKafkaProducer.start();
+            waitFor(30000);
+            streamKafkaProducer.stop();
+        } catch (Exception e) {
+            System.out.println("Error while producing stream producer");
+        }
+        ++count;
     }
 
     private static void waitFor(long waitTime) {
--- src/main/java/org/streaming/spring/core/StreamConsumer.java
+++ src/main/java/org/streaming/spring/core/StreamConsumer.java
@@ -4,4 +4,14 @@ package org.streaming.spring.core;
  * Created by syodage on 11/10/15.
  */
 public interface StreamConsumer {
+
+    /**
+     * Consume messages/requests.
+     */
+    void start() throws Exception;
+
+    /**
+     * Stop message consume
+     */
+    void stop() throws Exception;
 }
